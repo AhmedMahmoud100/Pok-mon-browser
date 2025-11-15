00649a6620a3ba9c79395d362f7e99c1943ace73
Ahmed <ahmedopo100@gmail.com>
2025-11-15 22:45:40 +0200

    feat: add icons

---
+++ src/assets/icons/ChevronLeft.tsx
@@ -0,0 +1,21 @@
+const ChevronLeft = () => {
+  return (
+    <svg
+      width="18"
+      height="15"
+      viewBox="0 0 24 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M14.5 15L9.5 10L14.5 5"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default ChevronLeft;
---
+++ src/assets/icons/ChevronRight.tsx
@@ -0,0 +1,21 @@
+const ChevronRight = () => {
+  return (
+    <svg
+      width="18"
+      height="15"
+      viewBox="0 0 24 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M9.5 5L14.5 10L9.5 15"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default ChevronRight;
