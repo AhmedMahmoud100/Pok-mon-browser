4488187c2e2e8f51b34d3b25506ff2451667daa0
Ahmed <ahmedopo100@gmail.com>
2025-11-16 02:02:28 +0200

    fix: increase spacing

--- src/pages/PokemonDetail/components/PokemonPhysicalStats.tsx
+++ src/pages/PokemonDetail/components/PokemonPhysicalStats.tsx
@@ -5,7 +5,7 @@ interface PokemonPhysicalStatsProps {
 
 const PokemonPhysicalStats = ({ height, weight }: PokemonPhysicalStatsProps) => {
   return (
-    <div className="grid grid-cols-2 gap-4 w-full">
+    <div className="grid w-full grid-cols-2 gap-4 mt-2">
       <div className="text-center">
         <div className="flex items-center justify-center mb-2">
           <svg className="w-5 h-5 text-(--color-text-muted) mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
